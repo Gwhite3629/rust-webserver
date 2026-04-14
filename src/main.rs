8c68ae74b60996950d84d7a356d4fd2f9d295c9b
gwhite3629 <grady.white@maine.edu>
2026-04-14 16:39:17 -0400

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -27,36 +27,25 @@ fn handle_connection(mut stream: TcpStream) {
     
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = match fs::read_to_string("hello.html") {
-            Ok(contents) => contents,
-            Err(error) => panic!("Error reading file: {error:?}"),
-        };
-        let length = contents.len();
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-
-        match stream.write_all(response.as_bytes()) {
-            Ok(result) => result,
-            Err(error) => panic!("Could not write response: {error:?}"),
-        };
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = match fs::read_to_string("404.html") {
-            Ok(contents) => contents,
-            Err(error) => panic!("Error reading file: {error:?}"),
-        };
-        let length = contents.len();
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
+    let contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(error) => panic!("Error reading file: {error:?}"),
+    };
+    let length = contents.len();
 
-        match stream.write_all(response.as_bytes()) {
-            Ok(result) => result,
-            Err(error) => panic!("Could not write response: {error:?}"),
-        };
-    }
+    let response = format!(
+        "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+    );
+
+    match stream.write_all(response.as_bytes()) {
+        Ok(result) => result,
+        Err(error) => panic!("Could not write response: {error:?}"),
+    };
+
 }
