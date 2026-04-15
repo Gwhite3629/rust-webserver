b0faf4e41264d8b8bf95681b741e0c9efe89e3bf
gwhite3629 <grady.white@maine.edu>
2026-04-14 21:36:53 -0400

    Added http methods

---
+++ src/method.rs
@@ -0,0 +1,48 @@
+
+
+#[derive(Eq, Hash, Clone)]
+pub enum HttpMethod {
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+    Other(String),
+}
+
+impl HttpMethod {
+    pub fn from_str(method: &str) -> HttpMethod {
+        let method = method.to_uppsercase();
+        match method.as_str() {
+            "GET" => HttpMethod::GET,
+            "HEAD" => HttpMethod::HEAD,
+            "POST" => HttpMethod::POST,
+            "PUT" => HttpMethod::PUT,
+            "DELETE" => HttpMethod::DELETE,
+            "CONNECT" => HttpMethod::CONNECT,
+            "OPTIONS" => HttpMethod::OPTIONS,
+            "TRACE" => HttpMethod::TRACE,
+            "PATCH" => HttpMethod::PATCH,
+            _ => Self::Other(method.to_string()),
+        }
+    }
+
+    pub fn to_str(&self) -> &str {
+        match self {
+            HttpMethod::GET => "GET",
+            HttpMethod::HEAD => "HEAD",
+            HttpMethod::POST => "POST",
+            HttpMethod::PUT => "PUT",
+            HttpMethod::DELETE => "DELETE",
+            HttpMethod::CONNECT => "CONNECT",
+            HttpMethod::OPTIONS => "OPTIONS",
+            HttpMethod::TRACE => "TRACE",
+            HttpMethod::PATCH => "PATCH",
+            HttpMethod::Other(method) => method.as_str(),
+        }
+    }
+}
